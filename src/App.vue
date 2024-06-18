f084ba14a1654ca52097b65c44ee2720efc2a5ad
belajar-vue-2 <aryaadi229@gmail.com>
2024-06-18 17:49:17 +0700

    Persiapan Project Untuk Pelajari Component Dan Props

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>Quiz Vue</h1>
+      <input type="text" class="input" placeholder="search...">      
+    </header>
+  </main>
+</template>
+
+<style>
+main {
+  max-width: 900px;
+  display: flex;
+  justify-content: center;
+  margin: 25px;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+header input {
+  padding: 8px;
+  background-color: rgba(115, 188, 191, 0.523);
+  border: none;
+  border-radius: 5px;
+}
+</style>
